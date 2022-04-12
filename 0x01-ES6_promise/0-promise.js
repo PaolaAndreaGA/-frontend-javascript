4fdbefa6a4b78b912e221538368b10abb0e63608
PaolaAndreaGA <paga0209@gmial.com>
2022-04-12 14:40:49 -0500

    0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+function getResponseFromAPI() {
+  const promise1 = new Promise((resolve, reject) => {
+    resolve('Success!');
+  });
+
+  promise1.then((value) => {
+    console.log(value);
+      // expected output: "Success!"
+  });
+}
