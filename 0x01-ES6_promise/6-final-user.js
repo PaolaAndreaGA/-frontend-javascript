0865b4b9ae5d5d784e99f46ab0c54c661ac9ed67
PaolaAndreaGA <paga0209@gmial.com>
2022-04-13 11:27:19 -0500

    6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,15 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const myUser = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
+    
+  const fileUser =  await uploadPhoto(firstName).catch((errordata) => ({
+    status: 'reject',
+    value: errordata.toString(),
+  }));
+  return Promise.resolve([myUser, fileUser]);
+}
