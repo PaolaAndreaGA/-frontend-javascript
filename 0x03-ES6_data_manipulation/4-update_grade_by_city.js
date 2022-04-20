277d8f8211cbad58fe8cb09fd6a1e02ce070cc4c
PaolaAndreaGA <paga0209@gmial.com>
2022-04-19 19:42:28 -0500

    ADD 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  if (Object.getPrototypeOf(students) !== Array.prototype){
+  if (Object.getPrototypeOf(students) !== Array.prototype) {
     return [];
   }
-  if (Object.getPrototypeOf(newGrades) !== Array.prototype){
+  if (Object.getPrototypeOf(newGrades) !== Array.prototype) {
     return [];
   }
   return students.filter((student) => student.location === city).map((student) => {
-    const [newGrades] = newGrades.filter((item) => item.studId === student.id);
+    const [newGrades] = newGrades.filter((item) => item.studentId === student.id);
     return {...student, grade: newGrades ? newGrades.grade: 'N/A'};
   });
 }
