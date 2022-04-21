450f229fba248a78c9619d0988ee9478298e473a
PaolaAndreaGA <paga0209@gmial.com>
2022-04-20 22:49:39 -0500

    [UPDATE] Creating an interface for a student

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -20,3 +20,20 @@ const secondStudent: Student = {
 }
 
 const studentsList: Array<Student> = [firstStudent, secondStudent]
+
+const newTable = document.createElement('table');
+const bodyTable = document.createElement('body');
+
+studentsList.forEach((obj) => {
+  const myrow = document.createElement('row');
+  const cellstName = document.createElement('tfn');
+  const cellLocation = document.createElement('tlc');
+  cellstName.textContent = obj.firstName;
+  cellLocation.textContent =obj.location;
+  myrow.appendChild(cellstName);
+  myrow.appendChild(cellLocation);
+  bodyTable.appendChild(myrow);
+});
+
+newTable.appendChild(bodyTable);
+document.body.appendChild(newTable);
