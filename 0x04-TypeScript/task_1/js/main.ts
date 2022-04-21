8fe9efb004b5c1297c9304b244ae543e59628405
PaolaAndreaGA <paga0209@gmial.com>
2022-04-20 21:50:54 -0500

    [UPDATE] 2. Extending the Teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,10 @@ interface Teacher {
   [propName : string]: any;
 }
 
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -14,5 +18,13 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
-
 console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'Juan',
+  lastName: 'Duarte',
+  location: 'Mississauga',
+  fullTimeEmployee: false,
+  numberOfReports: 15,
+};
+console.log(director1);
