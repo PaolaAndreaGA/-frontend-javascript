5b2665d46ae52536c7ad33d6d74b3658854cce44
PaolaAndreaGA <paga0209@gmial.com>
2022-04-22 21:42:38 -0500

    [UPDATE]fixed bugs main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -9,6 +9,6 @@ export interface MinorCredits {
 function sumMajorCredits(subject1: MajorCredits, subject2:MajorCredits): number{
   return subject1.credits + subject2.credits;
 }
-function sumMinorCreditssubject1: MajorCredits, subject2:MajorCredits): number{
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number{
   return subject1.credits + subject2.credits;
 }
