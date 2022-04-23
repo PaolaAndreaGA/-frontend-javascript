30ebf4f1d98a74bee5702ebb669a5095b779920b
PaolaAndreaGA <paga0209@gmial.com>
2022-04-22 21:38:35 -0500

    [ADD] Initial configuration / main.ts

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,14 @@
+export interface MajorCredits {
+  credits: number;
+}
+
+export interface MinorCredits {
+  credits: number;
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2:MajorCredits): number{
+  return subject1.credits + subject2.credits;
+}
+function sumMinorCreditssubject1: MajorCredits, subject2:MajorCredits): number{
+  return subject1.credits + subject2.credits;
+}
